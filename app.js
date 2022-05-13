4c781a22d55ba49e83b49605f099599fe7644102
theawes0megamer <98134376+theawes0megamer@users.noreply.github.com>
2022-05-12 22:48:38 -0600

    new version... been taking a break from coding
    been taking a break from a while, but will finish the website soon and launch it. just wait until schools out :)

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const menu = document.querySelector('#mobile-menu')
-const menuLinks = documents.querySelector ("'.navbar__menu'")
+const menuLinks = document.querySelector ('.navbar__menu')
 
 menu.addEventListener('click', function (){
-    menu.classList.toggle ('is-active')
+    menu.classList.toggle('is-active')
     menuLinks.classList.toggle('active')
 });
